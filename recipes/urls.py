601984bad7c40cdd629a60472ab2cf53bd3ea3b0
Alyson Dick <2084268D@student.gla.ac.uk>
2023-03-03 13:36:58 +0000

    More views and urls set up

--- recipes/urls.py
+++ recipes/urls.py
@@ -6,4 +6,7 @@ app_name = 'recipes'
 urlpatterns = [
     path('', views.home, name = 'home'),
     path('my_account/', views.my_account, name = 'my account'),
+    path('recipes/', views.recipes, name = 'recipes'),
+    path('faq/', views.faq, name = 'faq'),
+    path('about/', views.about, name = 'about'),
 ]
--- recipes/views.py
+++ recipes/views.py
@@ -8,3 +8,15 @@ def home(request):
 def my_account(request):
     context_dict = {'my_account_message' : 'Whatever is in my_account_message in my_account views.py'}
     return render(request, 'recipes/my_account.html', context=context_dict)
+
+def recipes(request):
+    context_dict = {'recipes_message' : 'Whatever is in recipes_message in my_account views.py'}
+    return render(request, 'recipes/recipes.html', context=context_dict)
+
+def faq(request):
+    context_dict = {'faq_message' : 'Whatever is in faq_message in my_account views.py'}
+    return render(request, 'recipes/faq.html', context=context_dict)
+
+def about(request):
+    context_dict = {'about_message' : 'Whatever is in about_message in my_account views.py'}
+    return render(request, 'recipes/about.html', context=context_dict)
